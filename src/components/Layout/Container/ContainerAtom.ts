62a25f6b5c3164821cdcdced4ed516372d3d73b1
Alaric Bell <alaric.bell@gmail.com>
2022-10-25 16:42:56 +0200

    Add project skeleton

---
+++ src/components/Layout/Container/ContainerAtom.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 95%;
+  max-width: 1400px;
+  margin: 0 auto;
+  padding-right: 15px;
+  padding-left: 15px;
+`;
+
+export const ContainerFluid = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding-right: 15px;
+  padding-left: 15px;
+`;
---
+++ src/enums/base/Variables.ts
@@ -0,0 +1,14 @@
+export enum Colors {
+  Primary = "#261275",
+  Secondary = "#3d2a83",
+  White = "#ffffff",
+  Dark = "#73d597",
+  Highlight = "#cc0051",
+}
+
+export enum Breakpoints {
+  Small = "576px",
+  Medium = "768px",
+  Large = "992px",
+  Ultra = "1200px",
+}
